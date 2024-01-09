92d37b2d7e4f931926963be4fb8316ca50578c5a
flippi321 <90700334+flippi321@users.noreply.github.com>
2024-01-09 21:28:51 +0100

    Implemented the new pages in the homepage navigation bar

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:squashmate/custom_assets/customnavigationbar.dart';
+import 'package:squashmate/match_history/matchhistorypage.dart';
+import 'package:squashmate/new_match/newmatchpage.dart';
+import 'package:squashmate/scoreboard/scoreboardpage.dart';
+import 'package:squashmate/user_profile/userprofilepage.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -35,6 +39,32 @@ class HomePageState extends State<HomePage> {
         onItemSelected: (index) {
           setState(() {
             _currentIndex = index;
+            switch (index) {
+              case 1:
+                // Navigate to Match History page
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const MatchHistoryPage()));
+                break;
+              case 2:
+                // Navigate to Scoreboard page
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const ScoreboardPage()));
+                break;
+              case 3:
+                // Navigate to New Match page
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const NewMatchPage()));
+                break;
+              case 4:
+                // Navigate to Feedback page
+                // Navigator.push(context, MaterialPageRoute(builder: (context) => FeedbackPage()));
+                break;
+              case 5:
+                // Navigate to My Profile page
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const UserProfilePage()));
+                break;
+              default:
+                // Navigate to Home page
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const HomePage()));
+                break;
+            }
           });
         },
         currentIndex: _currentIndex,
