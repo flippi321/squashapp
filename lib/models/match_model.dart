e0172848755674d1321232dab7b3d12f8dce9834
flippi321 <90700334+flippi321@users.noreply.github.com>
2024-01-09 22:41:48 +0100

    Updated the match_model to have a date variable

--- lib/models/match_model.dart
+++ lib/models/match_model.dart
@@ -1,27 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class MatchModel {
   final String? id;
-  final String player1id;
-  final String player2id;
+  final Timestamp matchDate; 
+  final String player1;
+  final String player2;
 
   const MatchModel({
     this.id,
-    required this.player1id,
-    required this.player2id,
+    required this.matchDate,
+    required this.player1,
+    required this.player2,
   });
 
   toJson() {
     return {
       "id": id,
-      "player1id": player1id,
-      "player2id": player2id,
+      "matchdate" : matchDate,
+      "player1": player1,
+      "player2": player2,
     };
   }
 
   factory MatchModel.fromJson(Map<dynamic, dynamic> json) {
     return MatchModel(
         id: json['id'] as String?,
-        player1id: json['player1id'] as String,
-        player2id: json['player2id'] as String,
+        matchDate: json['matchdate'] as Timestamp,
+        player1: json['player1'] as String,
+        player2: json['player2'] as String,
       );
   }
 }
