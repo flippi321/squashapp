b98f6697192a3b2c6d5ec6257e1583d523be2e9d
flippi321 <90700334+flippi321@users.noreply.github.com>
2024-01-09 21:53:03 +0100

    Overhauled the Navigation bar to both look and function better

--- lib/custom_assets/customnavigationbar.dart
+++ lib/custom_assets/customnavigationbar.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 
-class CustomNavigationBar extends StatelessWidget {
+class CustomBottomAppBar extends StatelessWidget {
   final Function(int) onItemSelected;
   final int currentIndex;
 
-  const CustomNavigationBar({
+  const CustomBottomAppBar({
     Key? key,
     required this.onItemSelected,
     required this.currentIndex,
@@ -12,48 +12,33 @@ class CustomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        BottomNavigationBar(
-          currentIndex: currentIndex,
-          onTap: onItemSelected,
-          selectedItemColor: Colors.blueAccent,
-          unselectedItemColor: Colors.black,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.history),
-              label: 'Match History',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.leaderboard),
-              label: 'Scoreboard',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.feedback),
-              label: 'Feedback',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: 'My Profile',
-            ),
-          ],
-        ),
-        Positioned(
-          bottom: 8.0, // Adjust the height as needed
-          left: MediaQuery.of(context).size.width / 2 - 28.0, // Center the button horizontally
-          child: ElevatedButton(
-            onPressed: () {
-              // Add your functionality for the circular button here
-              // You can use the same onItemSelected method or define a new one
-            },
-            style: ElevatedButton.styleFrom(
-              shape: const CircleBorder(),
-              padding: const EdgeInsets.all(16.0),
-            ),
-            child: const Icon(Icons.add, size: 28.0),
+    return BottomAppBar(
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          IconButton(
+            icon: const Icon(Icons.history),
+            onPressed: () => onItemSelected(1),
           ),
-        ),
-      ],
+          IconButton(
+            icon: const Icon(Icons.leaderboard),
+            onPressed: () => onItemSelected(2),
+          ),
+          FloatingActionButton(
+            child: const Icon(Icons.add, size: 30,),
+            onPressed: () => onItemSelected(3),
+          ),
+          IconButton(
+            icon: const Icon(Icons.feedback),
+            onPressed: () => onItemSelected(4),
+          ),
+          IconButton(
+            icon: const Icon(Icons.person),
+            onPressed: () => onItemSelected(5),
+          ),
+        ],
+      ),
     );
   }
 }
