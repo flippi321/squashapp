599ac245c7bbb427c799237285e81d4f94639bae
flippi321 <90700334+flippi321@users.noreply.github.com>
2024-01-11 17:35:57 +0100

    Updated login page to work with the backend

--- lib/screens/user_registration/loginpage.dart
+++ lib/screens/user_registration/loginpage.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:squashmate/screens/homepage.dart';
 import 'package:squashmate/screens/user_registration/registerpage.dart';
+import 'package:squashmate/services/auth_service.dart';
+import 'package:provider/provider.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,8 +12,9 @@ class LoginPage extends StatefulWidget {
 }
 
 class LoginPageState extends State<LoginPage> {
-  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   String _errorMessage = ''; // To store and display error messages
 
   @override
@@ -22,58 +25,76 @@ class LoginPageState extends State<LoginPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            if (_errorMessage.isNotEmpty)
-              Column(
-                children: [
-                  Text(
-                    _errorMessage,
-                    style: const TextStyle(color: Colors.red, fontSize: 14),
-                  ),
-                  const SizedBox(height: 16.0),
-                ],
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              if (_errorMessage.isNotEmpty)
+                Column(
+                  children: [
+                    Text(
+                      _errorMessage,
+                      style: const TextStyle(color: Colors.red, fontSize: 14),
+                    ),
+                    const SizedBox(height: 16.0),
+                  ],
+                ),
+              TextField(
+                controller: _emailController,
+                decoration: const InputDecoration(labelText: 'Email'),
               ),
-            TextField(
-              controller: _usernameController,
-              decoration: const InputDecoration(labelText: 'Username'),
-            ),
-            const SizedBox(height: 16.0),
-            TextField(
-              controller: _passwordController,
-              decoration: const InputDecoration(labelText: 'Password'),
-              obscureText: true,
-            ),
-            const SizedBox(height: 16.0),
-            ElevatedButton(
-              onPressed: () {
-                // Check if username or password is empty
-                if (hasEmptyCridentials()) {
-                  showError('Username and password are required.');
-                } else {
-                  // Reset error message
-                  showError('');
-                  // Continue with login logic
-                  Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => const HomePage()),
-                );
-                }
-              },
-              child: const Text('Login'),
-            ),
-            const SizedBox(height: 8.0),
-            TextButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const RegisterPage()),
-                );
-              },
-              child: const Text("No user? Create one here!"),
-            ),
-          ],
+              const SizedBox(height: 16.0),
+              TextField(
+                controller: _passwordController,
+                decoration: const InputDecoration(labelText: 'Password'),
+                obscureText: true,
+              ),
+              const SizedBox(height: 16.0),
+              ElevatedButton(
+                onPressed: () async {
+                  // Check if email or password is empty
+                  if (hasEmptyCridentials()) {
+                    showError('Email and password are required.');
+                  } else {
+                    // Reset error message
+                    showError('');
+
+                    // Login user
+                    if (_formKey.currentState!.validate()) {
+                      final authService = context.read<AuthService>();
+                      final errorMessage =
+                          await authService.signInUser(
+                              _emailController.text,
+                              _passwordController.text);
+                      if (errorMessage != null) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          SnackBar(content: Text(errorMessage)),
+                        );
+                      } else {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const HomePage()));
+                      }
+                    }
+                  }
+                },
+                child: const Text('Login'),
+              ),
+              const SizedBox(height: 8.0),
+              TextButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const RegisterPage()),
+                  );
+                },
+                child: const Text("No user? Create one here!"),
+              ),
+            ],
+          ),
         ),
       ),
     );
@@ -86,7 +107,8 @@ class LoginPageState extends State<LoginPage> {
     });
   }
 
-  bool hasEmptyCridentials(){
-     return (_usernameController.text.isEmpty || _passwordController.text.isEmpty);
+  bool hasEmptyCridentials() {
+    return (_emailController.text.isEmpty ||
+        _passwordController.text.isEmpty);
   }
 }
