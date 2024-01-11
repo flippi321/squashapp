a3adf73b82b9f47327fac77b54753d00c36eb20e
flippi321 <90700334+flippi321@users.noreply.github.com>
2024-01-11 18:07:56 +0100

    Fixed user page

--- lib/screens/user_profile/userprofilepage.dart
+++ lib/screens/user_profile/userprofilepage.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:squashmate/models/user_model.dart';
+import 'package:squashmate/services/auth_service.dart';
 
 class UserProfilePage extends StatefulWidget {
-  const UserProfilePage({super.key});
+  const UserProfilePage({Key? key}) : super(key: key);
 
   @override
   UserProfileState createState() => UserProfileState();
@@ -10,11 +14,40 @@ class UserProfilePage extends StatefulWidget {
 class UserProfileState extends State<UserProfilePage> {
   @override
   Widget build(BuildContext context) {
+    final authService = context.read<AuthService>();
+
+    // Get user information from AuthService
+    UserModel? user = authService.currentUser; // Assuming you have a User model
+
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Match History'),
+        title: const Text('Your Profile'),
       ),
-      body: const Text("This is the Match History Page")
+      body: user != null
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Display the user image (replace 'userImage' with your actual image)
+                  const Image(
+                    image: AssetImage('assets/images/default_image.jpg'),
+                  ),
+                  const SizedBox(height: 20),
+                  Text(user.username),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  // Display the user's first name, last name, and username
+                  Text('${user.firstName} ${user.lastName}'),
+                  const SizedBox(
+                    height: 100,
+                  ),
+                ],
+              ),
+            )
+          : const SizedBox(
+              height: 50,
+            ),
     );
   }
 }
