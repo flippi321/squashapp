10935117065b0a339bea3f0c1dc175c36c0d0a98
flippi321 <90700334+flippi321@users.noreply.github.com>
2024-01-11 17:07:51 +0100

    Added provider to the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
 import 'package:squashmate/screens/user_registration/loginpage.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:squashmate/services/auth_service.dart';
+import 'firebase_options.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ],
+  );
+
+  runApp(
+    const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => AuthService(),
+        ),
+        
+      ],
+      child: Builder(
+        builder: (context) {
+          return MaterialApp(
+            title: 'App',
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const LoginPage(),
+          );
+        },
       ),
-      home: const LoginPage(),
     );
   }
 }
