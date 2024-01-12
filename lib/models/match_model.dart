4d40d0322adec803cd4291cd98d9de8d480b9c92
flippi321 <90700334+flippi321@users.noreply.github.com>
2024-01-12 10:43:44 +0100

    Added scorechange values to the match model

--- lib/models/match_model.dart
+++ lib/models/match_model.dart
@@ -5,12 +5,16 @@ class MatchModel {
   final Timestamp matchDate; 
   final String player1;
   final String player2;
+  final int? p1scorechange;
+  final int? p2scorechange;
 
   const MatchModel({
     this.id,
     required this.matchDate,
     required this.player1,
     required this.player2,
+    this.p1scorechange,
+    this.p2scorechange,
   });
 
   toJson() {
@@ -19,6 +23,8 @@ class MatchModel {
       "matchdate" : matchDate,
       "player1": player1,
       "player2": player2,
+      "p1scorechange" : p1scorechange,
+      "p2scorechange" : p2scorechange,
     };
   }
 
@@ -28,6 +34,8 @@ class MatchModel {
         matchDate: json['matchdate'] as Timestamp,
         player1: json['player1'] as String,
         player2: json['player2'] as String,
+        p1scorechange: json['p1scorechange'] as int,
+        p2scorechange: json['p2scorechange'] as int,
       );
   }
 }
